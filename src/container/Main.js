1b634e94e21871199b12364933dc22bc5dcd6e5d
hyunuk <hus0316@ariatech.kr>
2021-05-24 15:05:22 +0900

    class component +

--- src/container/Main.js
+++ src/container/Main.js
@@ -1,34 +1,81 @@
-import React, { useEffect, useState } from 'react';
-import Header from '../component/Header'
-import Gnb from '../component/Gnb'
-import Footer from '../component/Footer'
+import React, { Component } from 'react';
 import axios from 'axios';
-import WebtoonList from '../component/WebtoonList';
 
-const Main = (props) => {
-    const [webtoonList, setWebtoonList] = useState([]);
-    const [dday, setDday] = useState('mon');
+import Header from "../component/Header";
+import Gnb from "../component/Gnb";
+import Footer from "../component/Footer";
+import WebtoonList from "../component/WebtoonList";
 
-    useEffect(() => {
-        axios.get('dummy/webtoon_list.json')
-            .then(res => setWebtoonList(res.data.webtoonList))
-            .catch(error => console.log(error))
-            console.log(props);
+class Main extends Component{
+    constructor(props){
+        super(props);
 
-    }, []);
-    useEffect(() => {
-        console.log(props)
-    }, [dday]);
-    return (
-        <div>
-            <Header />
-            <Gnb dday={dday}/>
-            <WebtoonList list={webtoonList.filter(webtoon => (
-                dday === webtoon.day
-            ))
-            } />
-            <Footer />
-        </div>
-    );
+        const query = new URLSearchParams(props.location.search);
+        const day = query.get('day');
+
+        this.state = {
+            day : day || 'mon', //디폴트로 월요일
+            webtoonList : [] //초기 리스트는 비어있습니다.
+        };
+    }
+
+    componentDidMount(){
+        this._getList();
+    }
+
+    componentDidUpdate(prevProps){
+        //요일이 바뀌면 다시 setState 처리
+        let prevQuery = new URLSearchParams(prevProps.location.search);
+        let prevDay = prevQuery.get('day');
+
+        let query = new URLSearchParams(this.props.location.search);
+        let day = query.get('day');
+
+        if(prevDay !== day){
+            this.setState({
+                day
+            })
+        };
+    }
+
+    _getList(){
+        //webtoon_list를 가지고 옵니다.
+        const apiUrl = '/dummy/webtoon_list.json';
+
+        axios.get(apiUrl)
+            .then(data => {
+                //가지고 온 리스트를 state에 저장합니다.
+                this.setState({
+                    webtoonList : data.data.webtoonList
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    render(){
+        return (
+            <div>
+                <Header />
+                <Gnb day={this.state.day} />
+
+                { this.state.webtoonList.length > 0 ? (
+                    <WebtoonList list={
+                        this.state.webtoonList.filter(webtoon => (
+                            webtoon.day === this.state.day
+                        ))
+                    } />
+                ) : (
+                    <span>
+                        LOADING...
+                    </span>
+                )}
+
+                <Footer />
+            </div>
+        )
+    }
 }
+
 export default Main;
